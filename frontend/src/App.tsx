51cea3025cdb339e1ef20dcd0c28a0da20755df8
Yuri Ferreira <ynogueiira@gmail.com>
2021-05-03 20:11:45 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
 import React from 'react';
 
 
 function App() {
   return (
     <>
-    <h1 className="text-primary">Hello World</h1>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Index</h1>
+        <DataTable />
+      </div>
+      <Footer />
     </>
   );
 }
